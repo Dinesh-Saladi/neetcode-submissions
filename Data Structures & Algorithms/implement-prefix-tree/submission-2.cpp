512adc227ccc1cefffae424727e6c69cb01a7ac9
Dinesh Saladi <139242506+Dinesh-Saladi@users.noreply.github.com>
2026-06-28 07:59:47 +0530

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.cpp
@@ -0,0 +1,53 @@
+#include <cstring>
+
+struct Node{
+    bool is;
+    Node* a[26];
+    Node(){
+        is = false;
+        memset(a, NULL, sizeof(a));
+    };
+};
+class PrefixTree {
+public:
+    Node* root;
+    PrefixTree() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* temp = root;
+        for(auto c : word){
+            int cc = c - 'a';
+            if(temp->a[cc]){
+                temp = temp->a[cc];
+            }else{
+                temp->a[cc] = new Node();
+                temp = temp->a[cc];
+            }
+        }
+        temp->is = true;
+    }
+    
+    bool search(string word) {
+        Node* temp = root;
+        for(auto c : word){
+            int cc = c - 'a';
+            if(temp->a[cc]){
+                temp = temp->a[cc];
+            }else return false;
+        }
+        return temp->is;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* temp = root;
+        for(auto c : prefix){
+            int cc = c - 'a';
+            if(temp->a[cc]){
+                temp = temp->a[cc];
+            }else return false;
+        }
+        return true;
+    }
+};
